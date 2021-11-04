312be25d610f688e969da01945ae8eb97c0cb914
Punit-Choudhary <itspunitchoudhary@gmail.com>
2021-11-04 14:24:37 +0530

    Change url finder regex

--- Cogs/on_message.py
+++ Cogs/on_message.py
@@ -19,7 +19,7 @@ class OnMessageCog(commands.Cog):
     async def on_message(self, message):
 
         # Don't reply/watch messages from bots
-        if message.author.bot:
+        if message.author.bot or message.author.guild_permissions.administrator:
             return
         
 
@@ -72,7 +72,8 @@ class OnMessageCog(commands.Cog):
         # Anti-Link
         if config['antiLink']:
             def check_link(message) -> bool:
-                urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9][$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F]))+', message.content.lower())
+                regex=r"\b((?:https?://)?(?:(?:www\.)?(?:[\da-z\.-]+)\.(?:[a-z]{2,6})|(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|(?:(?:[0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,7}:|(?:[0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,5}(?::[0-9a-fA-F]{1,4}){1,2}|(?:[0-9a-fA-F]{1,4}:){1,4}(?::[0-9a-fA-F]{1,4}){1,3}|(?:[0-9a-fA-F]{1,4}:){1,3}(?::[0-9a-fA-F]{1,4}){1,4}|(?:[0-9a-fA-F]{1,4}:){1,2}(?::[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:(?:(?::[0-9a-fA-F]{1,4}){1,6})|:(?:(?::[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(?::[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(?:ffff(?::0{1,4}){0,1}:){0,1}(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])|(?:[0-9a-fA-F]{1,4}:){1,4}:(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])))(?::[0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])?(?:/[\w\.-]*)*/?)\b"
+                urls = re.findall(regex, message.content.lower())
                 if urls:
                     return True
                 else:
